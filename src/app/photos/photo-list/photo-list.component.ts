cb80875e9aa757ab12713c946ef866839d845911
Ricardo da Silva Nunes <ricardorichsn@gmail.com>
2018-07-21 15:33:43 -0300

    Debounce pattern usando Subject

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -8,16 +10,22 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
-
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter = '';
+  debounce: Subject< string > = new Subject < string >();
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     /* data.photos é o valor que está sendo atribuido no arquivo de rotas pelo resolver criado */
     this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
   }
 
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 }
