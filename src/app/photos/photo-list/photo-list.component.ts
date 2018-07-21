8319c9e2bf3d793edb8643f3a68de5e82375d026
Ricardo da Silva Nunes <ricardorichsn@gmail.com>
2018-07-21 16:27:48 -0300

    Paginação com Load more button

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -14,10 +15,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter = '';
   debounce: Subject< string > = new Subject < string >();
+  hasMore = true;
+  currentPage = 1;
+  userName: '';
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService) { }
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     /* data.photos é o valor que está sendo atribuido no arquivo de rotas pelo resolver criado */
     this.photos = this.activatedRoute.snapshot.data.photos;
     this.debounce
@@ -27,5 +34,14 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) { this.hasMore = false; }
+      });
   }
 }
