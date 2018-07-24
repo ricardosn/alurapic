53c9fe31711b6156f875c53afb5be2917166d0ea
Ricardo da Silva Nunes <ricardorichsn@gmail.com>
2018-07-23 23:37:09 -0300

    Search component e EventEmitter

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -11,10 +9,9 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter = '';
-  debounce: Subject< string > = new Subject < string >();
   hasMore = true;
   currentPage = 1;
   userName: '';
@@ -27,19 +24,13 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     this.userName = this.activatedRoute.snapshot.params.userName;
     /* data.photos é o valor que está sendo atribuido no arquivo de rotas pelo resolver criado */
     this.photos = this.activatedRoute.snapshot.data.photos;
-    this.debounce
-    .pipe(debounceTime(300))
-    .subscribe(filter => this.filter = filter);
-  }
-
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
   }
 
   load() {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(photos => {
+        this.filter = '';
         this.photos = this.photos.concat(photos);
         if (!photos.length) { this.hasMore = false; }
       });
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 import { PhotoModule } from './photo/photo.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
+import { SearchComponent } from './photo-list/search/search.component';
 
 @NgModule({
     imports: [
         PhotoModule,
         PhotoFormModule,
-        PhotoListModule
+        PhotoListModule,
+        SearchComponent
     ]
 })
 export class PhotosModule {}
