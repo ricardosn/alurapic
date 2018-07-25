bb3b7cb8a3b6a2801fb07c79834cff012b66ba26
Ricardo da Silva Nunes <ricardorichsn@gmail.com>
2018-07-24 22:20:18 -0300

    Diretiva Darken On Hover

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,18 +7,22 @@ import { LoadButtonComponent } from './load-button/load-button.component';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from '../../shared/components/card/card.module';
+import { SearchComponent } from './search/search.component';
+import { DarkenOnHoverModule } from '../../shared/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
     declarations: [
         PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescription
+        FilterByDescription,
+        SearchComponent
     ],
     imports: [
         CommonModule,
         PhotoModule,
-        CardModule
+        CardModule,
+        DarkenOnHoverModule
     ]
 })
 export class PhotoListModule {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { PhotoModule } from './photo/photo.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
-import { SearchComponent } from './photo-list/search/search.component';
 
 @NgModule({
     imports: [
         PhotoModule,
         PhotoFormModule,
-        PhotoListModule,
-        SearchComponent
+        PhotoListModule
     ]
 })
 export class PhotosModule {}
