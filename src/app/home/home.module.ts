2cf14955b1352d0bd2b8122576920887eb21602a
Ricardo da Silva Nunes <ricardorichsn@gmail.com>
2018-07-29 01:05:32 -0300

    Consumir serviço de autenticação no login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { SignInComponent } from './signin/signin.component';
 import { CommonModule } from '../../../node_modules/@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '../../../node_modules/@angular/router';
 
 @NgModule({
     declarations: [SignInComponent],
     imports: [
         CommonModule,
         ReactiveFormsModule,
-        VMessageModule
+        VMessageModule,
+        RouterModule
     ]
 })
 export class HomeModule { }
