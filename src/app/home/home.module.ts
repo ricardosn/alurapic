762b02240b4eaddd723dc17c496fbb7703df6182
Ricardo da Silva Nunes <ricardorichsn@gmail.com>
2018-07-28 23:54:27 -0300

    Validação do formulário e componente VMessage

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
+import { CommonModule } from '../../../node_modules/@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
-    declarations: [SignInComponent]
+    declarations: [SignInComponent],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule
+    ]
 })
 export class HomeModule { }
