fc9156e13a4fd967b382ce4131d865fc9419b62b
Ricardo da Silva Nunes <ricardorichsn@gmail.com>
2018-08-04 00:41:51 -0300

    UserService login e token decode

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from '../../../../node_modules/rxjs/operators';
 
-import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -13,14 +13,14 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private tokenService: TokenService) { }
+    private userService: UserService) { }
 
   authenticate(userName: string, password: string) {
     return this.http
       .post(API_URL + '/user/login/', {userName, password}, {observe: 'response'})
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
-        this.tokenService.setToken(authToken);
+        this.userService.setToken(authToken);
         console.log(`user ${userName} autenticado com token ${authToken}`);
       }));
   }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { User } from '../user/user';
+import { UserService } from '../user/user.service';
 
 @Component({
     selector: 'ap-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+    user$: Observable<User>;
+
+    constructor(userService: UserService) {
+        this.user$ = userService.getUser();
+    }
+}
