ddf6493c4f4d8e5452cfb2f1fcfe8593f9854c52
Ricardo da Silva Nunes <ricardorichsn@gmail.com>
2018-08-04 00:59:11 -0300

    logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
@@ -11,7 +12,13 @@ import { UserService } from '../user/user.service';
 export class HeaderComponent {
     user$: Observable<User>;
 
-    constructor(userService: UserService) {
+    constructor(private userService: UserService,
+        private router: Router) {
         this.user$ = userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
